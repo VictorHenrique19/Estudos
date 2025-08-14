4e82120dfa3f6845f54394be6c5737bde4539742
Victor Henrique <vctrhenri017@gmail.com>
2025-08-14 17:47:26 -0300

    adicionei os tipos de vareaveis.

---
+++ Tipos de vareaveis/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        byte: -128 a 127 (1 byte ou 8 bit)
+        short: -32768 a 32767 (2 byte ou 16 bit)
+        int: -2147483648 a 2147483647 (2 bilhões) (4 bytes ou 32 bit)
+        long: −9223372036854775808 a 9223372036854775807 (8 bytes ou 64 bit)
+
+        Numeros decimeais
+
+        float: 7 casas decimais - 1234567 (4 bytes ou 32 bit)
+        double: 15 casas ddecimais- 123456789123456 (8 bytes ou 64 bit)
+
+        String: vai receber textos == palavras e frases
+        char: Suporte apenas um caracter
+
+         */
+
+        byte idade = 120;
+        
+        System.out.println(idade);
+
+
+    }
+}
