bb2fb19688dcc3a9bdc76b98972b837e16c6366c
Victor Henrique <vctrhenri017@gmail.com>
2025-08-14 11:17:47 -0300

    DeclareiVareavel

---
+++ Aprendendo Java/src/declararVareavel.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class declararVareavel {
+    public static void main(String[] args) {
+        //tipo dde dado + identificação + valor atribuido
+        String nome = "Victor Henrique";
+
+        System.out.println(nome);
+        //Funçao que mostra o que e declarei na vareavel.
+    }
+}
